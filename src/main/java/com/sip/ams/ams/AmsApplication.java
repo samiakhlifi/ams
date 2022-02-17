3d9c82828833b9b678e6d7798328fc4b329280d1
samiakhlifi <samia.khelifi@polytechnicien.tn>
2022-02-17 10:27:00 +0100

    vesion postgres

--- src/main/java/com/sip/ams/ams/AmsApplication.java
+++ src/main/java/com/sip/ams/ams/AmsApplication.java
@@ -3,12 +3,10 @@ package com.sip.ams.ams;
 import com.sip.ams.ams.controllers.ArticleController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.File;
 
 @SpringBootApplication
-@EnableSwagger2
 public class AmsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/sip/ams/ams/configuration/DatabaseConfig.java
+++ src/main/java/com/sip/ams/ams/configuration/DatabaseConfig.java
@@ -1,9 +1,14 @@
 package com.sip.ams.ams.configuration;
 
-import com.zaxxer.hikari.*;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import javax.sql.DataSource;
+
 @Configuration
 public class DatabaseConfig {
     @Value("${spring.datasource.url}")
